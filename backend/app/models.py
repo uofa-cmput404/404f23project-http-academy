dad138d7c43bca09ab5874113573bb854bcaf5d4
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-10-20 06:34:34 -0600

    Created and registered User model; Installed app

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    host = models.CharField(max_length=100)
+    displayName = models.CharField(max_length=100)
+    github = models.CharField(max_length=100)
+    profileImage = models.CharField(max_length=100)
