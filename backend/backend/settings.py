c5dc202ee05f2d60b36f9d5476c8f80ebc76fb8c
Matheus Duncan <duncanmatheus905@gmail.com>
2023-10-21 19:55:33 -0600

    Made some progress on CORS errors, but still WIP.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders', # Allow CORS requests
     'posts',
 ]
 
@@ -50,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Allow CORS requests
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -124,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Only allow CORS requests from the local frontend server
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
