b9cb92dd3824e20d850e011e35b8b6841519bc09
Matheus Duncan <duncanmatheus905@gmail.com>
2023-10-23 17:40:07 -0600

    Functioning AxiosInstance for calling local Django.

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -9,32 +9,8 @@ const axiosInstance = axios.create({
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
-        'Host': 'localhost:8000',
-        'Access-Control-Allow-Origin': 'http://localhost:3000/', // Remove this in prod as it's a security risk
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS', // this too
         accept: 'application/json',
     },
 });
-
-axiosInstance.interceptors.response.use(
-
-    // if response is received, return it directly
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        const originalRequest = error.config;
-
-        // Prevent infinite loops by preventing refresh token requests from being retried
-        if (
-            error.response.status === 401 &&
-            originalRequest.url === baseURL + 'token/refresh/'
-        ) {
-            // navigate to login page
-            window.location.href = '/login';
-            return Promise.reject(error);
-        }
-    }
-);
 
 export default axiosInstance;
