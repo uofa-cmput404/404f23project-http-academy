0492dadfed399e9edfc5d14c98809a57fd3bb58e
OgSamu <133722906+OgSamu@users.noreply.github.com>
2023-10-24 14:22:04 -0600

    Update posts.js

--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -1,49 +1,46 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/posts'
+import axios from 'axios';
 
+const baseUrl = 'http://localhost:3001/posts';
 
-//get multiple posts
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => {
-        return response.data
-      })
-}
-
-//get single post
-const get = (id) => {
-    const request = axios.get(`${baseUrl}/${id}`);
-    return request.then(response => {
-        return response.data
-      })
+  const request = axios.get(baseUrl);
+  return request.then((response) => {
+    return response.data;
+  });
 };
 
-//create single post
-const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => {
-        return response.data
-      })
-}
+const get = (id) => {
+  const request = axios.get(`${baseUrl}/${id}`);
+  return request.then((response) => {
+    return response.data;
+  });
+};
 
-//update post
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => {
+    return response.data;
+  });
+};
+
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => {
-        return response.data
-      })
-}
-
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => {
+    return response.data;
+  });
+};
 
 const remove = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`);
-    return request.then(response => response.data);
-  };
-  
-  export default {
-    getAll,
-    create, 
-    update,
-    get,
-    remove 
-  };
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+const postService = {
+  getAll,
+  create,
+  update,
+  get,
+  remove,
+};
+
+export default postService;
