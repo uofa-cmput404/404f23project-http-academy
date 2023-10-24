7fbc6f4e0f8c289db12f7eea81c890a2a3145b1a
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-10-24 03:46:43 -0600

    Clicking on a post now renders a default detailed post page

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "../css/Post.css"
+import { Link } from 'react-router-dom'
 
 export default function Post({ post }) {
+    const neutral = {
+        color: 'black',
+        textDecoration: 'none'
+    }
+
     return (
         <div className="card">
+            <Link to={`/post/${post.id}`} style={neutral}>
             <div className="card-body">
                 <h2 className="card-title">{post.title}</h2>
                 <p className="card-text">{post.content}</p>
             </div>
+          </Link>
         </div>
     );
 }
---
+++ frontend/src/pages/Detail.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Post from "../components/Post"
+
+export default function Detail() {
+  return (
+    <h1>Detailed Post</h1>
+  );
+}
