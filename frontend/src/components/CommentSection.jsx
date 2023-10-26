f9fa365eb7e142454e65acdf5405b72d59f45937
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-10-26 06:42:00 -0600

    You can now add comments to posts from the detail page.

--- frontend/src/components/CommentSection.jsx
+++ frontend/src/components/CommentSection.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axiosInstance from '../axiosInstance';
 import Comment from './Comment';
-// Add CSS 
+// TODO: Add CSS 
 
 export default function CommentSection() {
   const defaultComments = [
@@ -11,8 +11,27 @@ export default function CommentSection() {
     { id: 3, author: 1, comment: "One more comment" },
   ];
   const [comments, setComments] = useState(defaultComments);
+
+  // Manage state of the textarea to clear it after an author posts a comment
+  const [commentText, setCommentText] = useState('');
+
   const { id } = useParams();
 
+  const addComment = (commentText) => {
+    axiosInstance.post('posts/${id}/comments/', {
+      postId: id,
+      author: 1,
+      comment: commentText
+    }).then(response => {
+      console.log(response);
+      setComments([...comments, { id: response.data.id, author: 1, comment: commentText }]);
+      setCommentText('');
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+
+  // Grab comments from the database
   useEffect(() => {
     axiosInstance.get(`posts/${id}/comments/`).then(response => {
       // console.log(response);
@@ -28,6 +47,8 @@ export default function CommentSection() {
       {comments.map((comment) => (
         <Comment key={comment.id} comment={comment.comment} />
       ))}
+      <textarea id="comment" name="comment" rows="2" cols="35" value={commentText} onChange={(e) => setCommentText(e.target.value)}></textarea>
+      <button onClick={() => addComment(document.getElementById("comment").value)}>Post Comment</button>
     </div>
   );
 }
