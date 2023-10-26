7ceb432ce64dac4e5ba2cc63e19f85c2f705b4f8
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-10-26 04:40:56 -0600

    Delete button is functional.

---
+++ frontend/src/components/DeleteButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import axiosInstance from '../axiosInstance';
+import { useNavigate } from "react-router-dom";
+
+export default function DeleteButton({ id }) {
+  let navigate = useNavigate();
+  const handleDelete = () => {
+    axiosInstance.delete(`posts/${id}`).then(response => {
+      console.log(response);
+      navigate("/home");
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+}
--- frontend/src/components/DetailedPost.jsx
+++ frontend/src/components/DetailedPost.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/DetailedPost.css";
 import LikeButton from "../components/LikeButton";
+import DeleteButton from "../components/DeleteButton";
 
 export default function DetailedPost({ post, id }) {
   return (
@@ -9,6 +10,7 @@ export default function DetailedPost({ post, id }) {
         <h2 className="post-title">{post.title}</h2>
         <p className="post-text">{post.content}</p>
         <LikeButton id={id} />
+        <DeleteButton id={id} />
       </div>
     </div>
   );
