51c4dc7a52bd1bc42645240d9975e8fed2ae9851
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-11-08 16:17:00 -0700

    Edited jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -177,10 +177,10 @@ const config = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    //   "\\.pnp\\.[^\\/]+$"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
@@ -193,6 +193,7 @@ const config = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
 };
 
 module.exports = config;
