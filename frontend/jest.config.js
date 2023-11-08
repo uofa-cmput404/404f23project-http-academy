e635330b949d02b95afe3d90f2c7c9a62419a4fa
Glory Omosomwan <80617508+asvpglory@users.noreply.github.com>
2023-11-08 16:20:10 -0700

    Edited jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -177,10 +177,12 @@ const config = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    "/node_modules/",
-    //   "\\.pnp\\.[^\\/]+$"
-  ],
+  // transformIgnorePatterns: [
+  //   "/node_modules/",
+  //   //   "\\.pnp\\.[^\\/]+$"
+  // ],
+  transformIgnorePatterns: ["node_modules/(?!axios)"],
+
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
