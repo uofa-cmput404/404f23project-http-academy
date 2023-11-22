1af4d75bfaead64698a450436624537c8747a065
Rahul Korde <70373291+RahulKorde19@users.noreply.github.com>
2023-11-21 18:03:45 -0700

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,9 +25,9 @@ from drf_yasg.views import get_schema_view
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="404f23project-http-academy API",
       default_version='v1',
-      description="Test description",
+      description="04f23project-http-academy description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
