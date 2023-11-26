cfffececfadad76abfbf8d5769c651b0a5a9b370
OgSamu <133722906+OgSamu@users.noreply.github.com>
2023-11-26 01:36:50 -0700

    Update CommentPost.jsx
    updqted missing dependencies

--- frontend/src/components/CommentPost.jsx
+++ frontend/src/components/CommentPost.jsx
@@ -1,34 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import '../css/SharePost.css';
 import DetailedPost from './DetailedPost';
 
 export default function SharePost({ posts, users }) {
     const [post, setPost] = useState([]);
-    const [allUsers, setAllusers] = useState([])
+    const [allUsers, setAllusers] = useState([]);
+    const [postId, setPostId] = useState("");
 
-    const [postId, setPostId] = useState("")
+
+    const findAuthorForPost = useCallback((authorId) => {
+        return allUsers.find(user => user.id.includes(authorId));
+    }, [allUsers]);
+
     useEffect(() => {
-        console.log('post in comment gotten from modal', posts.post_id);
-        console.log('users in comment gotten from modal', users);
         const enrichedPosts = {
-            ...post,
-            authorDetails: findAuthorForPost(post.author)
+            ...posts,
+            authorDetails: findAuthorForPost(posts.author)
         };
         setPost(enrichedPosts);
-        setPostId(posts.post_id)
-        // console.log('post id type', typeof posts.post_id)
-        setAllusers(users)
-
-    }, []);
-
-
-    const findAuthorForPost = (postId) => {
-        return allUsers.find(user => user.id.includes(postId));
-    };
-
-
-
-
+        setPostId(posts.post_id);
+        setAllusers(users);
+    }, [posts, users, findAuthorForPost]); 
 
     return (
         <div className='outer-container'>
